04e9e75acb2c71e83d98897a216e1a5f97457a87
TheHeadcrabFarmer <headcrab.farmer@gmail.com>
2021-01-29 19:30:55 +0100

    Project put in

---
+++ Assets/Scripts/GuyMover.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuyMover : MonoBehaviour
+{
+	public Rigidbody RigidBody;
+
+	public KeyCode Up, Down, Left, Right;
+
+	public float Speed;
+
+	private float horizontal, Vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
